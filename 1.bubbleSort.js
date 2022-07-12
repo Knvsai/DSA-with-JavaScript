d9311e09c3894586aba25f5bda296f3c263f35ff
Knvsai <knvsai0007@gmail.com>
2022-07-12 23:11:26 +0530

    day-1

---
+++ 1.bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(array) {
+    // Only change code below this line
+    for(var i=0;i<array.length-1;i++){
+        for(var j=0;j<array.length-1-i;j++){
+            if(array[j]>array[j+1]){
+                [array[j],array[j+1]]=[array[j+1],array[j]]
+            }
+        }
+    }
+    return array;
+    // Only change code above this line
+  }
+  console.log(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
